876f6fa2cd75b9770df31f800ee9f7f5ade1bce6
Sharllack <164765027+Sharllack@users.noreply.github.com>
2025-03-15 16:20:56 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal(".skill", {
+  delay: 300,
+  reset: true,
+  distance: "50px",
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".swiper", {
+  delay: 300,
+  reset: true,
+  distance: "50px",
+  origin: "bottom",
+});
