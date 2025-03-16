5ce7679bfec30a7be96dc1fd4812f8410090701c
Sharllack <164765027+Sharllack@users.noreply.github.com>
2025-03-15 21:49:48 -0300

    ok

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ ScrollReveal().reveal(".swiper", {
   distance: "50px",
   origin: "bottom",
 });
+
+var typed = new Typed("#desc", {
+  strings: [
+    "Desenvolvedor Full Stack",
+    "Especialista em Sistemas Web",
+    "Criador de Soluções Digitais",
+    "Apaixonado por Tecnologia",
+  ],
+  typeSpeed: 30, // Velocidade da digitação
+  backSpeed: 30, // Velocidade ao apagar
+  loop: true, // Fica repetindo
+});
+
+document.getElementById("ano-atual").textContent = new Date().getFullYear();
