c2925eca523db4ca79a98c078a8aa7648bf51379
Sharllack <164765027+Sharllack@users.noreply.github.com>
2025-03-21 02:45:39 -0300

    Atualizando css

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ ScrollReveal().reveal(".swiper", {
   origin: "bottom",
 });
 
+ScrollReveal().reveal("#sobre", {
+  delay: 300,
+  reset: true,
+  distance: "50px",
+  origin: "bottom",
+});
+
 var typed = new Typed("#desc", {
   strings: [
     "Desenvolvedor Full Stack",
@@ -46,4 +53,35 @@ $("#menu").on("click", function () {
   }
   // Alterna o estado do menu
   isMenuOpen = !isMenuOpen;
+});
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    const div = document.getElementById("descer");
+    div.style.display = "block"; // Torna a div visível
+    setTimeout(() => {
+      div.style.opacity = 1; // Torna a div gradualmente visível
+    }, 50); // Delay muito pequeno para permitir que a transição comece
+  }, 3000); // 3000 milissegundos = 3 segundos
+});
+
+$(document).on("scroll", function () {
+  const div = document.getElementById("descer");
+  const header = document.querySelector("#cabecalho");
+  const opc = document.querySelector("#opc");
+  if (window.scrollY > 0) {
+    div.style.display = "none";
+    div.style.opacity = 0;
+    header.style.padding = "5px";
+    opc.style.top = "58px";
+    opc.style.height = "calc(100% - 58px)";
+  } else {
+    div.style.display = "block";
+    setTimeout(() => {
+      div.style.opacity = 1;
+    }, 50);
+    header.style.padding = "10px";
+    opc.style.top = "68px";
+    opc.style.height = "calc(100% - 68px)";
+  }
 });
